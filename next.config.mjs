1affba6177190dadc3d55ec27a95864eab2b0ecd
DSaha14 <dishasaha1404@gmail.com>
2024-09-09 19:28:07 +0530

    initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
-
+module.exports = {
+    output: 'export', // Enables static export mode
+    // Add other configuration as needed
+  };
 export default nextConfig;
